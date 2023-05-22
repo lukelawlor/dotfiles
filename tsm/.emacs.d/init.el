1e26941fa4e82d2af1fb9be4d2571b8e7145222c
Luke Lawlor <lklawlor1@gmail.com>
2023-05-22 15:41:07 -0400

    added emacs config

---
+++ tsm/.emacs.d/init.el
@@ -0,0 +1,17 @@
+(setq make-backup-files nil)
+(setq auto-save-default nil)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(blink-cursor-mode nil)
+ '(org-startup-folded t)
+ '(org-startup-truncated nil)
+ '(tool-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "fixed" :foundry "misc" :slant normal :weight normal :height 105 :width normal)))))
