eefc8afe5c7fa9a73377de433c6806bf7439bcb3
Luke Lawlor <lklawlor1@gmail.com>
2023-05-22 22:01:18 -0400

    added evil mode config to init.el

--- tsm/.emacs.d/init.el
+++ tsm/.emacs.d/init.el
@@ -1,5 +1,7 @@
 (setq make-backup-files nil)
 (setq auto-save-default nil)
+(setq display-line-numbers-type 'relative)
+(setq-default frame-title-format '("emacs@something:%b"))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -8,10 +10,25 @@
  '(blink-cursor-mode nil)
  '(org-startup-folded t)
  '(org-startup-truncated nil)
- '(tool-bar-mode nil))
+ '(package-selected-packages '(key-chord evil))
+ '(tool-bar-mode nil)
+ '(global-display-line-numbers-mode 1))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(default ((t (:family "fixed" :foundry "misc" :slant normal :weight normal :height 105 :width normal)))))
+(require 'package)
+(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(package-initialize)
+;; evil
+(require 'evil)
+(evil-mode 1)
+(require 'key-chord)
+(key-chord-mode 1)
+(setq key-chord-two-keys-delay 0.5)
+(key-chord-define evil-insert-state-map "jj" 'evil-normal-state)
+;; emacs keys
+(global-set-key (kbd "C-j") (lambda nil (interactive) (other-window 1)))
+(global-set-key (kbd "C-k") (lambda nil (interactive) (other-window -1)))
