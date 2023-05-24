0220ea35f3af5076f584e6656b00a172a416fcf1
Luke Lawlor <lklawlor1@gmail.com>
2023-05-24 00:41:50 -0400

    added org latex config

--- tsm/.emacs.d/init.el
+++ tsm/.emacs.d/init.el
@@ -8,11 +8,17 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(blink-cursor-mode nil)
+ '(evil-undo-system 'undo-redo)
+ '(global-display-line-numbers-mode 1)
+ '(org-format-latex-options
+   '(:foreground default :background default :scale 1.2 :html-foreground "Black" :html-background "Transparent" :html-scale 1.0 :matchers
+		 ("begin" "$1" "$" "$$" "\\(" "\\[")))
+ '(org-preview-latex-image-directory "/tmp/ltximg/")
  '(org-startup-folded t)
  '(org-startup-truncated nil)
+ '(org-startup-with-inline-images t)
  '(package-selected-packages '(key-chord evil))
- '(tool-bar-mode nil)
- '(global-display-line-numbers-mode 1))
+ '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -24,11 +30,11 @@
 (package-initialize)
 ;; evil
 (require 'evil)
-(evil-mode 1)
+;;(evil-mode 1)
 (require 'key-chord)
 (key-chord-mode 1)
 (setq key-chord-two-keys-delay 0.5)
 (key-chord-define evil-insert-state-map "jj" 'evil-normal-state)
 ;; emacs keys
-(global-set-key (kbd "C-j") (lambda nil (interactive) (other-window 1)))
-(global-set-key (kbd "C-k") (lambda nil (interactive) (other-window -1)))
+;;(global-set-key (kbd "C-j") (lambda nil (interactive) (other-window 1)))
+;;(global-set-key (kbd "C-k") (lambda nil (interactive) (other-window -1)))
